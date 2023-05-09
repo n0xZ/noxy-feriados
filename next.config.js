fe3bad3657f25c27cd2bc33d050bde338aa23e7a
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-09 20:24:10 -0300

    chore: expose env variables on next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	env: {
+		API_URL: process.env.API_URL,
+		RAPID_API_HOST: process.env.RAPID_API_HOST,
+		RAPID_API_KEY: process.env.RAPID_API_KEY,
+	},
+}
 
 module.exports = nextConfig
