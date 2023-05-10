069b0559db3cc41b2d6f08535ed7ef009840b7cc
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-09 22:09:42 -0300

    chore: setup home page with new styles and features related to locales w/ vercel headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { Holiday } from '@/types'
 import { headers } from 'next/headers'
-const getNextHoliday = async () => {
-	const country = headers().get('x-vercel-ip-country')
+
+async function getNextHoliday() {
+	const country = headers().get('x-vercel-ip-country')?.toUpperCase() ?? 'AR'
+	const city = headers().get('x-vercel-ip-city') ?? 'Buenos Aires'
+	const userLocation = `${city} (${country})`
 
 	const actualDate = new Date()
 	const API_URL = process.env.API_URL ?? ''
 	const RAPID_API_HOST = process.env.RAPID_API_HOST ?? ''
 	const RAPID_API_KEY = process.env.RAPID_API_KEY ?? ''
-	const res = await fetch(`${API_URL}/${actualDate.getFullYear()}/AR`, {
+	const res = await fetch(`${API_URL}/${actualDate.getFullYear()}/${country}`, {
 		method: 'GET',
 		headers: {
 			'X-RapidAPI-Key': RAPID_API_KEY,
@@ -23,14 +26,26 @@ const getNextHoliday = async () => {
 			new Date(h.date).getDay() > actualDate.getDay()
 	)
 
-	return { nextHoliday, country }
+	return { nextHoliday, userLocation }
 }
 export default async function Home() {
-	const holiday = await getNextHoliday()
-	console.log(holiday.country)
+	const result = await getNextHoliday()
+
 	return (
-		<main className="container flex flex-col items-center justify-center h-screen max-w-4xl mx-auto space-y-3">
-			<h2>El próximo feriado es: {holiday.nextHoliday?.localName}</h2>
-		</main>
+		<>
+			<main className="container flex flex-col items-center justify-center h-screen max-w-4xl mx-auto space-y-3 text-xl antialiased text-center xl:text-3xl">
+				<h2 className="xl:text-4xl text-2xl text-[#0284c7]">
+					El próximo feriado en {result.userLocation} es:
+				</h2>
+				<p>{result.nextHoliday?.localName}</p>
+				<time className="text-lg text-gray-400" dateTime={result.nextHoliday?.date.toString()}>
+					{result.nextHoliday?.date.toString()}
+				</time>
+			</main>
+
+			<footer className="p-5 text-center text-[#4a4a4a]">
+				<p>©️ Noxy, 2023.</p>
+			</footer>
+		</>
 	)
 }
