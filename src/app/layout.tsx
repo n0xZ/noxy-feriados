c6d5ffdf4297a8f8dd3942b3f1fc61b0cce55977
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-09 22:09:12 -0300

    chore: setup global layout styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[#0f0f0f] text-[#fdfdfd]`}>{children}</body>
     </html>
   )
 }
